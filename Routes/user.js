dad38c01f8bbb5bbc200e3c020af0d6b731875f6
Iuri071 <iuri_matiello@estudante.sesisenai.org.br>
2021-10-26 23:30:22 -0300

    urlEncoded-OK

--- Routes/user.js
+++ Routes/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
-const User = mongoose.model('User', { nome: String, email: String, senha: String, senhaConfirm: String })
-const axios = require('axios').create({ baseURL: "http://localhost:3031/" })
+const axios = require('axios').create({ baseURL: "http://localhost:3030/" })
 
-mongoose.connect('mongodb+srv://Iuri:admin123@cluster0.ndtwj.mongodb.net/test')
 
-function UserRoute(app) {
+const userRoute = (app) => {
+    
+    const User = mongoose.model('User', { nome: String, email: String, senha: String, senhaConfirm: String })
+    mongoose.connect('mongodb+srv://Iuri:admin123@cluster0.ndtwj.mongodb.net/test')
+
     app.route('/usuario')
-        .get((req, res) => {
+        .post((req, res) => {
             const { nome, email, senha, senhaConfirm } = req.body
             const newUser = new User(req.body)
             if (nome != null && email != null && senha != null && senhaConfirm != null) {
@@ -22,19 +24,20 @@ function UserRoute(app) {
             }
         })
 
+
     app.route('/usuario/login/')
         .get(async (req, res) => {
-            const { email, senha } = req.query
-            const usuarios = await User.find({ email, senha })
+            const { email, senha } = req.body
+            const usuarios = await User.find(email, senha)
             res.status(200, 'Ok').json(usuarios)
         })
 
     app.route('/usuario/remove/')
-        .get(async (req, res) => {
+        .delete(async (req, res) => {
             const { nome, email, senha, senhaConfirm } = req.body
-            const usersDel = await User.remove({ nome })
+            const usersDel = await User.remove(nome)
             res.status(202, 'Accepted').json(usersDel)
         })
 }
 
-module.exports = UserRoute;
+module.exports = userRoute;
--- app.js
+++ app.js
@@ -1,53 +1,19 @@
-const UserRoute = require('./Routes/user')
+const userRoute = require('./Routes/user')
 const { json } = require('express')
 const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const PORT = process.env.PORT || 3031
+const PORT = process.env.PORT || 3030
 
 var http = require('http')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-UserRoute(app)
-
-app.use(express.json())
-
-// const User = mongoose.model('User', { nome: String, email: String, senha: String, senhaConfirm: String })
-// mongoose.connect('mongodb+srv://Iuri:admin123@cluster0.ndtwj.mongodb.net/test')
-// app.use(require('cors')())
+userRoute(app)
 
 app.get('/', (req, res) => {
     res.status(200, 'Ok').json('Backend rodando')
 })
-
-// app.post('/usuario', (req, res) => {
-//     const { nome, email, senha, senhaConfirm } = req.body
-//     const newUser = new User(req.body)
-//     if (nome != null && email != null && senha != null && senhaConfirm != null) {
-//         newUser.save(req.body)
-//             .then((result) => {
-//                 res.json(result)
-//             }).catch((error) => {
-//                 res.json(error)
-//             })
-//         return res.status(201, 'Created').json(req.body)
-//     } else {
-//         return res.status(400, 'Bad request').json('erro no post')
-//     }
-// })
-
-// app.get('/usuario/login/', async (req, res) => {
-//     const { email, senha } = req.body
-//     const usuarios = await User.find(email, senha)
-//     res.status(200, 'Ok').json(usuarios)
-// })
-
-// app.delete('/usuario/remove/', async (req, res) => {
-//     const { nome, email, senha, senhaConfirm } = req.body
-//     const usersDel = await User.remove(nome)
-//     res.status(202, 'Accepted').json(usersDel)
-// })
 
 var server = http.createServer(app)
 server.listen(PORT)
