f3510da02beff25dc7d3f35130cc3786376c13bb
Iuri071 <iuri_matiello@estudante.sesisenai.org.br>
2021-10-28 22:54:26 -0300

    Yup ready to merge

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -1,39 +1,41 @@
 const mongoose = require('mongoose')
 const axios = require('axios').create({ baseURL: 'http://localhost:3030/' })
 const User = require('../Schemas/Mongo')
+const Controller = require('../Schemas/Validation')
 
 mongoose.connect('mongodb+srv://Iuri:admin123@cluster0.ndtwj.mongodb.net/test')
 
 const userRoute = (app) => {
 
-    app.route('/usuario')
-        .post((req, res) => {
-            const { nome, email, senha, senhaConfirm } = req.body
-            const newUser = User(req.body)
-            if (nome != null && email != null && senha != null && senhaConfirm != null) {
-                newUser.save(req.body)
-                return res.status(201).json(User)
-            } else {
-                return res.status(400).json('Erro no POST')
-            }
-        })
+    app.post('/usuario', Controller.store)
+        // .post((req, res) => {
+        //     const { nome, email, senha, senhaConfirm } = req.body
+        //     const newUser = User(req.body)
+        //     if (nome != null && email != null && senha != null && senhaConfirm != null) {
+        //         newUser.save(req.body)
+        //         return res.status(201).json(User)
+        //     } else {
+        //         return res.status(400).json('Erro no POST')
+        //     }
 
-    app.route('/usuario/login/').get(async (req, res) => {
-        const { email, senha } = req.body
-        const usuarios = await User.find(email, senha)
-            .then((usuarios) => {
-                res.status(200).json(usuarios)
-            }).catch((err) => {
-                res.status(400).json(err + "Erro no GET")
-            })
-    })
 
-    app.route('/usuario/remove/')
-        .delete(async (req, res) => {
-            const { nome, email, senha, senhaConfirm } = req.body
-            const usersDel = await User.remove(nome, email, senha, senhaConfirm)
-            res.status(202).json(usersDel)
-        })
+    app.get('/usuario/login/', Controller.store)
+    // .get(async (req, res) => {
+    //     const { email, senha } = req.body
+    //     const usuarios = await User.find(email, senha)
+    //         .then((usuarios) => {
+    //             res.status(200).json(usuarios)
+    //         }).catch((err) => {
+    //             res.status(400).json(err + "Erro no GET")
+    //         })
+    // })
+
+    app.delete('/usuario/remove/', Controller.store)
+        // .delete(async (req, res) => {
+        //     const { nome, email, senha, senhaConfirm } = req.body
+        //     const usersDel = await User.remove(nome, email, senha, senhaConfirm)
+        //     res.status(202).json(usersDel)
+        // })
 }
 
 module.exports = userRoute;
--- src/Schemas/Validation.js
+++ src/Schemas/Validation.js
@@ -1,29 +1,31 @@
 const mongoose = require("mongoose")
 const Yup = require("yup")
-const User = require("./Mongo/User")
+// const User = require("./Mongo/User")
 
-const Controller = async (req, res) => {
+const Controller = {
 
-    const userController = Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().email().required(),
-        senha: Yup.string().required().min(6),
-        senhaConfim: Yup.string().required().min(6)
-    })
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            senha: Yup.string().required().min(6),
+            senhaConfim: Yup.string().required().min(6)
+        })
 
-    if (!(await schema.isValid(req.body))) {
-        return res.status(400).json({ mensagem: 'Erro na validação dos campos' })
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ mensagem: 'Erro na validação dos campos' })
+        }
+
+        const userExists = await User.findOne({ email: req.body.email })
+
+        if (userExists) {
+            return res.status(400).json({ mensagem: 'Usuário já existe no sistema' })
+        }
+
+        const { nome, email } = await User.create(req.body)
+
+        return res.status(201).json({ nome, email })
     }
-
-    const userExists = await User.findOne({ email: req.body.email })
-
-    if (userExists) {
-        return res.status(400).json({ mensagem: 'Usuário já existe no sistema' })
-    }
-
-    const { nome, email } = await User.create(req.body)
-
-    return res.status(201).json({ nome, email })
 }
 
 module.exports = Controller;
