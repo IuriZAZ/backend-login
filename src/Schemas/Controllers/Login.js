5ce08cd3f0e724947a8b8f71bee4a638d0d938ce
Iuri071 <iuri_matiello@estudante.sesisenai.org.br>
2021-11-04 19:59:32 -0300

    Talvez funcione, talvez não, mas testa aí

--- src/Schemas/Controllers/Login.js
+++ src/Schemas/Controllers/Login.js
@@ -4,29 +4,36 @@ const Yup = require("yup")
 const User = require("../Mongo")
 
 const ControllerLog = {
-    async store(req,res){
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            senha: Yup.string().required(),
-          })
-      
-          if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ mensagem: 'Erro ao validar os campos' })
-          }
-      
-          console.log(req.body)
-          const { email, senha } = req.body
-      
-          const User = await User.findOne({ email })
-      
-          if (!User) {
-            return res.status(401).json({ mensagem: 'Usuario nao encontrado' })
-          }
-      
-          if (!(await User.comparePassword(senha))) {
-            return res.status(401).json({ mensagem: 'senha nao confere' })
-          }
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      senha: Yup.string().required(),
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ mensagem: 'Erro ao validar os campos' })
     }
+
+    console.log(req.body)
+    const { email, senha } = req.body
+
+    const User = await User.findOne({ email })
+
+    if (!User) {
+      return res.status(401).json({ mensagem: 'Usuario nao encontrado' })
+    }
+
+    if (!(await User.comparePassword(senha))) {
+      return res.status(401).json({ mensagem: 'senha nao confere' })
+    }
+
+    const { _id } = user
+    return res.status(201).json({
+      token: jwt.sign({ _id, email }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      })
+    })
+  }
 }
 
 module.exports = ControllerLog;
