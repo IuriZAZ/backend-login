98a93e9cef51c1a596b240f775439f8486e6da3e
Iuri N. Matiello <82899922+IuriDev071@users.noreply.github.com>
2021-11-05 11:00:54 -0300

    Update mongo.js

--- src/Schemas/mongo.js
+++ src/Schemas/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 // https://blog.betrybe.com/desenvolvimento-web/yup/
-
+// https://www.npmjs.com/package/yup
 const schemaModel = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true },
