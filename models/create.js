f5ad256db30dd7809b2522d846100e8551a40e00
Iuri071 <iuri_matiello@estudante.sesisenai.org.br>
2021-11-16 21:18:43 -0300

    Sequelize do céu

--- models/create.js
+++ models/create.js
@@ -1,36 +1,66 @@
 const mysql = require('mysql')
+const Sequelize = require('sequelize')
 const database = require('./models')
 
-// ODS / Nota Fiscal
-const ods_insert = database.define('servico', {
-    id_servico: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    nome_usuario: {
-        type: database.STRING,
-        allowNull: false
-    },
-    data_entrada: {
-        type: database.DATEONLY,
-        allowNull: false
-    },
-    data_saida: {
-        type: database.DATEONLY,
-        allowNull: false
-    },
-    preco_mobra: {
-        type: database.DOUBLE,
-        allowNull: false
-    },
-    preco_peca: {
-        type: database.DOUBLE,
-        defaultValue: '0'
-    },
-    valor_total: {
-        type: database.INTERGER,
-        values: [preco_mobra + preco_peca]
-    }
-})
+
+const CreateDB = async () => {
+
+    // Table -> Servico
+    const servico = await Sequelize.define('servico', {
+        id_servico: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        data_entrada: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        data_saida: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        descricao: {
+            type: Sequelize.STRING,
+        },
+        preco_mobra: {
+            type: Sequelize.DOUBLE,
+            allowNull: false
+        },
+        preco_peca: {
+            type: Sequelize.DOUBLE,
+            defaultValue: '0'
+        },
+    })
+
+
+    // Table -> Usuario
+    const usuario = await Sequelize.define('usuario', {
+        id_usuario: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: 'servico',
+                foreignKey: id_servico,
+                foreignKeyConstraint: true
+            }
+        },
+        nome_usuario: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email_usuario: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        senha_usuario: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    })
+}
+
+module.exports = CreateDB;
